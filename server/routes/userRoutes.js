39fc676b899e7f3ed5d6529592cdd53bede964dd
hariprasathselvamani <hariprasaths1331@gmail.com>
2025-09-20 17:06:32 +0530

    Add server folder

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { userAuth } from "../middleware/userAuth.js";
-import { getUserData } from "../controllers/userController.js"; // ✅ Add `.js` here too if it's a local file
-
-const userRouter = express.Router();
-
-userRouter.get("/data", userAuth, getUserData);
-
-export default userRouter;
