8a4b012964c20557c0bba94fc611b49f475708c8
hariprasathselvamani <hariprasaths1331@gmail.com>
2025-09-20 14:16:09 +0530

    Update gitignore, index, and server config

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173", // local frontend
-  process.env.FRONTEND_URL, // deployed frontend
+   // deployed frontend
 ].filter(Boolean);
 app.use(
   cors({
@@ -23,13 +23,15 @@ app.use(
 
 app.get("/", (req, res) => res.send("API Working"));
 
-app.use((req, res, next) => {
-    res.status(404).json({ success: false, message: "API route not found" });
-});
+
 
 // Mount routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).json({ success: false, message: "API route not found" });
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
